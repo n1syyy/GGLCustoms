b211ca4e43b4dbf27b6bcb49ae404dd3f640c097
Nik <sysuevdp11@gmail.com>
2022-08-20 17:36:20 +0300

    Added domain to hosts

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'gglcustoms.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'gglcustoms.herokuapp.com', 'www.gglcustoms.shop', 'gglcustoms.shop']
 
 
 # Application definition
